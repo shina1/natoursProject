0070b2f55107e770c67363001f297f73956b98d3
shina1 <oswasiu@gmail.com>
2020-11-09 12:41:15 +0200

    complete APIs

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ const app = express();
 //------------------- middleware functions ------------------
 app.use(express.json());
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware function');
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+// console.log(process.env);
 //------------------------ server -------------------------
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`app running on port ${port}`);
 });
