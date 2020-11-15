c5c32b3aaea615cea5383f27284a7603d83b3325
shina1 <oswasiu@gmail.com>
2020-11-16 01:49:57 +0200

    made the API better

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,11 +5,51 @@ const Tour = require('./../Model/tourModel');
 //   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 // );
 
-// route handlers for tours
+// route handlers for tours 
+// i.e the handlers to perform all the crud operations
 exports.getAllTours =async (req, res) => {
 try {
-  console.log(req.query);
-  const toursData = await Tour.find();
+  
+  // build the query
+  // 1a) Filtering
+  const queryObj = {...req.query};
+  // we exclude the el in the array below so as not to affect our filtering API
+  const excludedFields = ['page','sort','limit','fields'];
+ excludedFields.forEach(el => delete queryObj[el]);
+// 1b) Advanced Filtering
+let queryStr = JSON.stringify(queryObj);
+queryStr = queryStr.replace(/\b(gte|gt|lte|le)\b/g, match=>`$${match}`)
+
+ let queryDB =  Tour.find(JSON.parse(queryStr));
+//  remeber Tour.find(JSON.parse(queryStr)) will always return a query
+//  2) Sorting
+if(req.query.sort){
+  const sortBy = req.query.sort.split(",").join(" ");
+  queryDB = queryDB.sort(sortBy);
+}else{
+  queryDB = queryDB.sort('-createdAt');
+}
+// 3) field limiting
+if(req.query.fields){
+  const fields = req.query.fields.split(",").join(" ");
+  console.log(fields);
+  queryDB = queryDB.select(fields);
+}else{
+  queryDB = queryDB.select('-__v')
+}
+// 4) Pagiation
+const page = req.query.page * 1 || 1;
+const limit = req.query.limit * 1 || 100;
+const skip = (page - 1) * limit;
+
+queryDB = queryDB.skip(skip).limit(limit);
+if(req.query.page){
+  const numTours = await Tour.countDocuments();
+  if(skip >= numTours) throw new Error('This page does not exist');
+}
+//  execute the query
+  const toursData = await queryDB;
+  // send response
   res.status(200).json({
     status: 'success',
     results: toursData.length,
@@ -21,8 +61,7 @@ try {
   res.status(404).json({
     status: "failed",
     message : err
-  })
-  
+  })  
 }
 };
 
@@ -46,7 +85,6 @@ exports.createTuors = async (req, res) => {
   }
 };
 exports.getTourById = async(req, res) => {
-  console.log(req.params.id)
   // const tourId = toursData.find((el) => el.id === id);
 
   try {
